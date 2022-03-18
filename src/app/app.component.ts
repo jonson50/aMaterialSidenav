332129cb0f7b373c28e0d952dceb3e4c762b56db
John Bermeo <john.bermeo@gmail.com>
2022-03-18 19:27:58 +0100

    blueprint working but need more customisation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'blueprint1';
+export class AppComponent implements AfterViewInit, OnInit {
+   @ViewChild(MatSidenav)
+   sidenav!: MatSidenav;
+
+   longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
+
+   constructor(private observer: BreakpointObserver) {}
+
+   ngOnInit(): void {}
+
+   ngAfterViewInit() {
+      // Solucion para cuando se cambia un valor en un componente del DOM
+      /* setTimeout(()=>{
+         this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+            if (res.matches) {
+               this.sidenav.mode = 'over';
+               this.sidenav.close();
+            } else {
+               this.sidenav.mode = 'side';
+               this.sidenav.open();
+            }
+         });
+      },0); */
+      // Otra solucion al mismo problema
+      Promise.resolve().then(() => {
+         this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+            if (res.matches) {
+               this.sidenav.mode = 'over';
+               this.sidenav.close();
+            } else {
+               this.sidenav.mode = 'side';
+               this.sidenav.open();
+            }
+         });
+      });
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Angular Material Modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [AppComponent],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      MatToolbarModule,
+      MatSidenavModule,
+      MatButtonModule,
+      MatIconModule,
+      MatDividerModule,
+      LayoutModule,
+      MatCardModule,
+      MatProgressBarModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
